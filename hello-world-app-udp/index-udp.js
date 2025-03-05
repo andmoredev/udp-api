94e0129f93f8c3013b7be3795956bd890f887cff
Andres Moreno <andres@andmore.dev>
2025-03-05 07:56:20 -0700

    do with a loop

--- hello-world-app-udp/index-udp.js
+++ hello-world-app-udp/index-udp.js
@@ -13,18 +13,32 @@ udpServer.on('message', (msg, rinfo) => {
   const responseMessage = 'Hello from UDP server!';
   console.log(`Hello from UDP server!`);
 
-  // wait 2 seconds before sending the response
-  setTimeout(() => {
-    console.log(`Sending UDP response: ${responseMessage}`);
+  const messages = [
+    msg.toString(),
+    'Hello from UDP server! 1111',
+    'Hello from UDP server! 2222',
+    'Hello from UDP server! 3333',
+  ];
 
-    udpServer.send(responseMessage, rinfo.port, rinfo.address, (err) => {
-      if (err) {
-        console.error('Error sending UDP response:', err);
-      }
-    });
-  }, 1000);
+  messages.forEach((message) => {
+    setTimeout(() => {
+      console.log(`Sending UDP response: ${message}`);
 
+      udpServer.send(message, rinfo.port, rinfo.address, (err) => {
+        if (err) {
+          console.error('Error sending UDP response:', err);
+        }
+      });
+    }, 200);
+  });
 
+  // console.log(`Sending UDP response: ${responseMessage}`);
+
+  // udpServer.send(responseMessage, rinfo.port, rinfo.address, (err) => {
+  //   if (err) {
+  //     console.error('Error sending UDP response:', err);
+  //   }
+  // });
 });
 
 udpServer.on('error', (err) => {
