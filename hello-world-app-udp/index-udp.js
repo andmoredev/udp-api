8b01f290ee5ca89f4a63d73a6cf299c35bd7e5d2
Andres Moreno <andres@andmore.dev>
2025-03-05 08:03:06 -0700

    HARDCODE MESSAGE

--- hello-world-app-udp/index-udp.js
+++ hello-world-app-udp/index-udp.js
@@ -20,17 +20,23 @@ udpServer.on('message', (msg, rinfo) => {
     'Hello from UDP server! 3333',
   ];
 
-  messages.forEach((message) => {
-    setTimeout(() => {
-      console.log(`Sending UDP response: ${message}`);
-
-      udpServer.send(message, rinfo.port, rinfo.address, (err) => {
-        if (err) {
-          console.error('Error sending UDP response:', err);
-        }
-      });
-    }, 200);
+  udpServer.send('WHAT IS HAPPENING', rinfo.port, rinfo.address, (err) => {
+    if (err) {
+      console.error('Error sending UDP response:', err);
+    }
   });
+
+  // messages.forEach((message) => {
+  //   setTimeout(() => {
+  //     console.log(`Sending UDP response: ${message}`);
+
+  //     udpServer.send(message, rinfo.port, rinfo.address, (err) => {
+  //       if (err) {
+  //         console.error('Error sending UDP response:', err);
+  //       }
+  //     });
+  //   }, 200);
+  // });
 
   // console.log(`Sending UDP response: ${responseMessage}`);
 
